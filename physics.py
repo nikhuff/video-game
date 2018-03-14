ff0143a14b108ad4f5fc46aaf54cef3189d1a09b
nikhuff <nik.huff@me.com>
2018-03-13 23:37:13 -0600

    added physics and point classes

---
+++ physics.py
@@ -0,0 +1,11 @@
+class Velocity():
+    def __init__(self):
+        self.dx = 0
+        self.dy = 0
+
+class Point():
+    def __init(self, x, y):
+        self.x = x
+        self.y = y
+
+        
