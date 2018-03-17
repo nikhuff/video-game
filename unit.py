4d60fdb43aa65069f3cb2bb8a539ef1c372bad14
nikhuff <nik.huff@me.com>
2018-03-17 17:12:45 -0600

    fleshed out unit class

--- unit.py
+++ unit.py
@@ -1,22 +1,70 @@
-from physics import *
+import pygame
 
+from physics import *
+from graphics import Graphics
 
 class Unit():
     def __init__(self, point):
         self.point = point
         self.velocity = Velocity()
-    
+        self.frame = 0.0
+        self.height = 0
+        self.width = 0
+
     def update(self):
-        self.point.x += self.velocity.dx
-        self.point.y += self.velocity.dy
+        self.point.setx(self.point.x + self.velocity.dx, self.width)
+        self.point.sety(self.point.y + self.velocity.dy, self.height)
 
 class Character(Unit):
     def __init__(self, point):
         super(Character, self).__init__(point)
+        self.spritesheet = Graphics.load("./assets/george.png")
+        self.mapping = self.get_mapping()
+        self.height = 33
+        self.width = 24
+        self.facing = "down"
 
-    def render(self):
-        pass
+    def update(self):
+        super(Character, self).update()
+        self.frame = (self.frame + self.velocity.get_speed()) % 4
+ 
+    def render(self, surface):
+        surface.blit(self.spritesheet,
+                     (self.point.x, self.point.y, self.width, self.height),
+                     self.mapping[self.facing][int(self.frame)])
 
+    def get_mapping(self):
+        return {
+            "up": [(24 * i, 0, 24, 33) for i in range(4)],
+			"right": [(24 * i, 33, 24, 33) for i in range(4)],
+			"down": [(24 * i, 66, 24, 33) for i in range(4)],
+			"left": [(24 * i, 99, 24, 33) for i in range(4)]
+        }
+
+    def set_direction(self):
+        if self.velocity.dx > 0:
+            self.facing = "right"
+        elif self.velocity.dx < 0:
+            self.facing = "left"
+        elif self.velocity.dy > 0:
+            self.facing = "down"
+        elif self.velocity.dy < 0:
+            self.facing = "up"
+
+    def handler(self, event):
+        self.velocity.dx, self.velocity.dy = 0, 0
+        if event.type == pygame.KEYDOWN:
+            if (event.key == pygame.K_UP or event.key == pygame.K_RIGHT or event.key == pygame.K_DOWN or event.key == pygame.K_LEFT):
+                if event.key == pygame.K_UP:
+                    self.velocity.dy = -1
+                elif event.key == pygame.K_DOWN:
+                    self.velocity.dy = 1
+                elif event.key == pygame.K_LEFT:
+                    self.velocity.dx = -1
+                elif event.key == pygame.K_RIGHT:
+                    self.velocity.dx = 1
+            self.set_direction()
 
 if __name__ == '__main__':
     unit = Unit(Point(0,0))
+    char = Character(Point(0,0))
