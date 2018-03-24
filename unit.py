b7b1aea2442ed4326dffa97d61e8d6d5b508cd67
Micah Rogers <micahrogers@hotmail.com>
2018-03-24 15:11:23 -0700

    added a little prototype of battle stuff. his a,s,d or click on the boxes to see a little print() mess age

--- unit.py
+++ unit.py
@@ -21,14 +21,14 @@ class Character(Unit):
         super(Character, self).__init__(point)
         self.spritesheet = Graphics.load("./assets/characters/ranger.png")
         self.mapping = self.get_mapping()
-        self.height = 33
+        self.height = 32
         self.width = 24
         self.facing = "down"
 
     def update(self):
         super(Character, self).update()
         self.frame = (self.frame + self.velocity.get_speed()) % 4
- 
+
     def render(self, surface):
         # make sure that character is not stuck in an animated frame if he is motionless
         if self.velocity.get_speed() == 0:
@@ -85,6 +85,29 @@ class Character(Unit):
                     self.velocity.setdx(1)
                     #audio.footsteps.play()
             self.set_direction()
+
+            if (event.key == pygame.K_a  or event.key == pygame.K_s or event.key == pygame.K_d):
+                if event.key == pygame.K_a:
+                    print("Welcome to punch town, population...You!")
+                elif event.key == pygame.K_s:
+                    print("Sup my homie?")
+                elif event.key == pygame.K_d:
+                    print("Fly, you fools!")
+
+
+        elif pygame.mouse.get_pressed()[0] == True:
+            x,y = pygame.mouse.get_pos()
+
+            if x > 195 and x < 310 and y > 645 and y < 685:
+                print("Welcome to punch town, population...You!")
+
+            elif x > 395 and x < 510 and y > 645 and y < 685:
+                print("Sup my homie?")
+
+            elif x > 600 and x < 715 and y > 645 and y < 685:
+                print("Fly, you fools!")
+
+
 
 if __name__ == '__main__':
     unit = Unit(Point(0,0))
